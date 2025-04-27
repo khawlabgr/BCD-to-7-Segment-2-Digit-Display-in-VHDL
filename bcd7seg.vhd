f4506ede14c08202b1f93b15df9e3d1a763427f8
khawla bougherara <125137694+khawlabgr@users.noreply.github.com>
2025-04-27 17:43:24 +0200

    Update bcd7seg.vhd

--- bcd7seg.vhd
+++ bcd7seg.vhd
@@ -13,10 +13,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_ARITH.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+
 
 entity bcd7seg is
     Port ( x : in  STD_LOGIC_VECTOR (3 downto 0);
